33710f45bffaec21d52cb046a77ecdb1efaa381e
Julián Omodey <julianomodey@hotmail.com>
2023-03-26 19:20:49 -0300

    Extract 404

---
+++ src/components/NotFoundPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from '../global/Layout'
+
+export const NotFoundPage = () => {
+  return (
+    <Layout>
+      <h1>Not found Page</h1>
+    </Layout>
+  )
+}
