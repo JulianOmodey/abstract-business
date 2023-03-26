6785e3a4d477fa2dbbe5713ce8612be52844bbd7
Julián Omodey <julianomodey@hotmail.com>
2023-03-26 19:40:36 -0300

    test

--- src/components/NotFoundPage/index.tsx
+++ src/components/NotFoundPage/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 import { Layout } from '../global/Layout'
+import * as Styled from './styled'
 
 export const NotFoundPage = () => {
   return (
     <Layout>
-      <h1>Not found Page</h1>
+      <Helmet>
+        <title>Not Found</title>
+      </Helmet>
+      <Styled.Title>Not found Page</Styled.Title>
     </Layout>
   )
 }
